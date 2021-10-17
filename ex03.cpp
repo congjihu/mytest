d3462141a926feb605ab949cde0120c4b9e387b7
congjihu <40128366+congjihu@users.noreply.github.com>
2021-10-17 18:42:09 +0800

    Add files via upload

---
+++ ex03.cpp
@@ -0,0 +1,27 @@
+/**
+Create a program that opens a file and counts the whitespace-separated words in that file.
+**/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    int counter = 0;
+    ifstream inputFile("03.txt");
+    string buf;
+
+	while (inputFile >> buf)
+	{
+		++counter;
+	}
+    
+	cout << "Whitespace-separated words: " << counter << endl;
+
+
+return 0;
+}
+// 统计文件03.txt 中的单词数
